65ed28150da68d06e39bda8268247dd80f1a1a9d
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2021-04-12 13:03:12 +0530

    link fixed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // app.use(express.static('public'));
 app.use(express.static(__dirname + '/public'));
-// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 app.get('/', (req, res)=>{
