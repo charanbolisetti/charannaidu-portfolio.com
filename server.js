80342bbe0861e7023f36551f1b1330a2e450cf17
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2021-04-12 11:15:30 +0530

    link fix

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
+const path = require('path')
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static('public'));
-// app.use(express.static(__dirname + '/public'));
-// app.use('/static', express.static(path.join(__dirname, '/public')))
+// app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+// app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 app.get('/', (req, res)=>{
