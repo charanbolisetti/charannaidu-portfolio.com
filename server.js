10500e07860f2a6d4ee2e2d54ad5aad106420387
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2021-04-12 11:08:43 +0530

    link fix

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const nodemailer = require('nodemailer');
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+// app.use(express.static(__dirname + '/public'));
+// app.use('/static', express.static(path.join(__dirname, '/public')))
 app.use(express.json())
 
 app.get('/', (req, res)=>{
