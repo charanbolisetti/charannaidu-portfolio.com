69e2aec51d780f4d5cb46dc4a20e901848cebdc4
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2021-04-12 12:59:43 +0530

    link fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.post('/', (req, res)=>{
